7ca2c95aadbe0329d3e0fc829468c1e4c6f93c03
Ayman Khafifi <khafifiayman318@gmail.com>
2026-02-22 06:08:38 +0000

    Sync LeetCode submission Runtime - 14 ms (32.34%), Memory - 46.5 MB (28.03%)

---
+++ 0125-valid-palindrome/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        for(int i = 0; i<cleaned.length()/2;i++){
+            if(cleaned.charAt(i)!=cleaned.charAt(cleaned.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
