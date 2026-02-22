5ff75ecaca3ea15aff46a17c740d98ca7204af26
Ayman Khafifi <khafifiayman318@gmail.com>
2026-02-22 06:02:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.8 MB (52.53%)

---
+++ 0141-linked-list-cycle/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+        ListNode slow = head, fast = head.next;
+
+        while(fast!=null && fast.next!=null){
+            if(slow==fast){
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+
+        }
+        return false;
+
+    }
+}
