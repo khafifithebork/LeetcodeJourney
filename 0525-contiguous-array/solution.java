3a8dcd1f434085bf52f6ba9be2cb98be112a73f3
Ayman Khafifi <khafifiayman318@gmail.com>
2026-04-19 13:08:58 +0000

    Sync LeetCode submission Runtime - 25 ms (21.47%), Memory - 65.6 MB (30.61%)

---
+++ 0525-contiguous-array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int sum =0, maxLen=0;
+        map.put(0,-1);
+        for(int i=0;i<nums.length;i++){
+            sum += (nums[i] == 0) ? 1 : -1;
+            if(map.containsKey(sum)) maxLen = Math.max(maxLen, i-map.get(sum));
+            else map.put(sum,i);
+        }
+        return maxLen;
+    }
+}
