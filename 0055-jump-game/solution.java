e63e8dcdfa12c310839283a992651b8ed7ffd80e
Ayman Khafifi <khafifiayman318@gmail.com>
2026-05-04 23:51:49 +0000

    Sync LeetCode submission Runtime - 2 ms (87.07%), Memory - 47.6 MB (90.51%)

---
+++ 0055-jump-game/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxJump=0;
+        for(int i=0;i<nums.length;i++){
+            if(maxJump<i) return false;
+            maxJump = Math.max(maxJump,nums[i]+i);
+            if(maxJump >= nums.length-1) return true;
+        }
+        return true;
+    }
+}
