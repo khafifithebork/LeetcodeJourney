ac87418cccab3aaf1118eb0cff79b0b2d367a2b9
Ayman Khafifi <khafifiayman318@gmail.com>
2026-05-29 19:38:58 +0000

    Sync LeetCode submission Runtime - 34 ms (46.47%), Memory - 94.9 MB (26.49%)

---
+++ 1966-frequency-of-the-most-frequent-element/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l = 0;
+        long sum = 0;
+        int maxLen = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+
+            while ((long)nums[r] * (r - l + 1) > sum + k) {
+                sum -= nums[l];
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+}
