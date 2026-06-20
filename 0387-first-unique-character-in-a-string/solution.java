5fcc575510ab65fb5d2964bd18ff71663af4dd19
Ayman Khafifi <khafifiayman318@gmail.com>
2026-06-20 20:30:43 +0000

    Sync LeetCode submission Runtime - 29 ms (43.67%), Memory - 46.9 MB (71.35%)

---
+++ 0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) {
+    Map<Character,Integer> map = new LinkedHashMap<>();
+       for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) +1);
+       }
+
+       for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1){
+                return s.indexOf(entry.getKey());
+            }
+       }
+       return -1;
+    }
+}
