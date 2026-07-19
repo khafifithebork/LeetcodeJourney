3144e7947996e6ed60c864fdf261a851e14f8d86
Ayman Khafifi <khafifiayman318@gmail.com>
2026-07-19 01:28:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (16.34%)

---
+++ 1217-relative-sort-array/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        count = [0] * 1001
+        for arr in arr1 :
+            count[arr]+=1
+        result = []
+        for num in arr2 :
+            result.extend([num] * count[num])
+            count[num]=0
+        for num in range(1001) :
+            if count[num] > 0 :
+                result.extend([num] * count[num])
+                count[num] = 0
+        return result
