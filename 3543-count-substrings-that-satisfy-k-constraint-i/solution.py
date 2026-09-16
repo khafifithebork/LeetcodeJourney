2ba07851e3a4a3b7a16eada18d87b9fa9ee35929
Ayman Khafifi <khafifiayman318@gmail.com>
2026-09-16 10:19:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (66.54%)

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        count, l, zeros, ones = 0, 0, 0, 0
+        for r in range(len(s)) :
+            if s[r] == '0' : zeros += 1 
+            else : ones += 1
+            while zeros > k and ones > k :
+                if s[l] == '0' : zeros -= 1 
+                else : ones -= 1
+                l += 1
+            count += r - l + 1
+        return count
